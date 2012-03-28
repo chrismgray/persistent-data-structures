2b15254163a33688c9c5d781d96a39d94c8cd17c
Chris Gray <chrismgray@gmail.com>
2012-03-28 09:48:39 -0600

    project.clj for CLJS

--- project.clj
+++ project.clj
@@ -1,3 +1,12 @@
 (defproject persistent-data-structures "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.4.0-alpha2"]])
+  :dependencies [[org.clojure/clojure "1.4.0-alpha2"]]
+  :cljsbuild {
+              :source-path "src-cljs"
+              :compiler {
+                         :output-to "foo"
+                         :optimizations :whitespace
+                         :pretty-print true
+                         }
+              }
+  :dev-dependencies [[lein-marginalia "0.7.0"]])
